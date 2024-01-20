2d7e5f6dac787c751b49ee3a412a33af4cf93140
girianimesh <animeshgiri98839828@gmail.com>
2024-01-20 19:05:32 +0530

    card added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import { Slider } from "./Components/Slider";
+import { Data } from "./Components/Data";
+import { useEffect, useState } from "react";
+import {Card} from "./Components/Card";
 function App() {
+  const [category, setcategory] = useState([]);
+  useEffect(() =>{
+    filter("");
+  },[]);
+  const filter = (names) => {
+    const filtered = Data.filter((item) => item.name.includes(names));
+    console.log(filtered);
+    setcategory(filtered);
+  };
   return (
     
     <div className="App">
-      <Slider/>
+      <Slider/>    
+      <div style={{display:'flex',flexWrap: "wrap"}}>
+        {category.map((item) => (
+        <Card
+          image={item.image}
+          Name={item.Name}
+          about={item.about}
+          actualPrice={item.actualPrice}
+          offerPrice={item.offerPrice}
+        />
+        ))}
+      </div>
     </div>
   );
 }
