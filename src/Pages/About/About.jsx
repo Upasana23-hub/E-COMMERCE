8745feefc2ac9f358d08b355d3523eec247d5195
Upasana Chattopadhyay <chattopadhyayupasana2@gmail.com>
2024-02-01 21:34:56 +0530

    About file created

---
+++ src/Pages/About/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './About.css'
+const About = () => {
+    return(
+        <>
+            <div className="outer-div">
+                <div className="image">
+                </div>
+            </div>
+        </>
+    )
+}
+export default About;
