52cb6f032dfbca1ebbbe204a27d65d9ee519c98f
Ankit Dhar <ankitdhar757@gmail.com>
2024-02-02 22:54:44 +0530

    second slider added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,13 +3,14 @@ import './Home.Css'
 import Homecard from "../Components/Homecard"
 import Product from "../Components/Product"
 import Slider from "../Components/Slider"
+import { Small } from "../Components/Small_slider/Small";
 const Home = () => {
     return(
         <>
         <Product/>
         <Slider/>
         <Homecard/>
-       
+        <Small/>
        
 
         </>
