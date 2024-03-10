c337155089ad63f54e0c4788b9b84830c7960acb
girianimesh <animeshgiri98839828@gmail.com>
2024-03-10 21:25:47 +0530

    filtered completed

--- src/Pages/Products/Another.jsx
+++ src/Pages/Products/Another.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './Another.css';
-import { useEffect } from "react";
 import { Data } from "../../Components/Data";
-import {Card} from "../../Components/Card";
+import { Card } from "../../Components/Card";
 import { useLocation } from 'react-router-dom';
 import { Link } from "react-router-dom";
+
 const Another = () => {
-
-  //   const [category, setcategory] = useState([]);
-  //   useEffect(() =>{
-  //   filter("");
-  // },[]);
-  // arrow function
-  // const filter = (names) => {
-  //   const filtered = Data.filter((item) => item.name.includes(names));
-  //   console.log(filtered);
-  //   setcategory(filtered);
-  // };
-
   const location = useLocation();
-  
-  console.log(location.search)
-    useEffect(() => {
-        if (!location.hash) {
-            window.scrollTo(0, 0);
-        }
-    }, [location]);
+  const searchParams = new URLSearchParams(location.search);
+  const searchTerm = searchParams.get('search');
+
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    if (Data && Data.length > 0 ) {
+      const filtered = Data.filter(item =>
+        item.name && item.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(Data);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (!location.hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
   return (
     <>
-      <div style={{display:'flex',flexWrap: "wrap"}} className='card-out'>
-            {Data.map((item) => (
-            <Link to={`/Single/${item.id}`}>
-              <Card
+      <div style={{ display: 'flex', flexWrap: "wrap" }} className='card-out'>
+        {filteredData.map(item => (
+          <Link to={`/Single/${item.id}`} key={item.id}>
+            <Card
               image={item.image}
               Name={item.Name}
               about={item.about}
@@ -40,12 +43,11 @@ const Another = () => {
               size={item.size}
               off={item.off}
             />
-            </Link>
-            
-            ))}
-            </div>
+          </Link>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Another
+export default Another;
