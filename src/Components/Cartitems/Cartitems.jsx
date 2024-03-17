fe8cad6a983fbbb7ea7ef69acb58604ab7d155ee
Subhratanu <subhratanu.jisce@gmail.com>
2024-03-18 02:09:42 +0530

    cart-fix - halfway commit

--- src/Components/Cartitems/Cartitems.jsx
+++ src/Components/Cartitems/Cartitems.jsx
@@ -1,75 +1,90 @@
-// // import React, { useContext } from 'react';
-// import './Cartitems.css';
-// import { Data } from '../Data';
-// // import { useState } from 'react';
-// import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-// // import Single from '../../Pages/Products/Single';
+import React, { useState, useEffect } from "react";
+import "./Cartitems.css";
+import { Data } from "../Data";
+// import { useState } from 'react';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+// import Single from '../../Pages/Products/Single';
 
-// const Cartitems = (cartItems,removefromcart) => {
-//     // const {cartItems,removefromcart}= useContext(Single)
-//     // Create Empty cart
-//     // const getDefaultCart = () => {
-//     //     let cart = {};
-//     //     for (let index = 0; index < Data.length + 1; index++) {
-//     //         cart[index] = 0;
-//     //     }
-//     //     return cart;
-//     // };
+const Cartitems = (cartItems, removefromcart) => {
+  // const {cartItems,removefromcart}= useContext(Single)
+  // Create Empty cart
+  // const getDefaultCart = () => {
+  //     let cart = {};
+  //     for (let index = 0; index < Data.length + 1; index++) {
+  //         cart[index] = 0;
+  //     }
+  //     return cart;
+  // };
 
-//     // const [cartitems, setcartitems] = useState(getDefaultCart());
+  // const [cartitems, setcartitems] = useState(getDefaultCart());
 
-//     // const removefromcart = (itemid) => {
-//     //     setcartitems((prev) => ({ ...prev, [itemid]: prev[itemid] - 1 }));
-//     // };
-//     console.log(cartItems.cartItems);
+  // const removefromcart = (itemid) => {
+  //     setcartitems((prev) => ({ ...prev, [itemid]: prev[itemid] - 1 }));
+  // };
+  // console.log(cartItems.cartItems);
+  const [items, setItems] = useState([]);
 
-//     return (
-//         <div className='cartitems'>
-//             <div className="cartitems-format-main">
-//                 <p>Products</p>
-//                 <p>Title</p>
-//                 <p>Price</p>
-//                 <p>Quantity</p>
-//                 <p>Total</p>
-//                 <p>Remove</p>
-//             </div>
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
 
-//             <hr />
-//             {Data.map((e) => {
-//                 if ((cartItems.cartItems[e.id]) > 0 ) 
-//                 {
-//                     return <div>                
-//                             <div className="cartitems-format">
-//                                 <img src={e.image} alt='' className='carticon-product-icon' />
-//                                 <p>{e.Name}</p>
-//                                 <p> ₹{e.offerPrice}</p>
-//                                 <button className='cartitems-quantity'>{cartItems.cartItems[e.id]}</button>
-//                                 <p>{e.offerPrice * cartItems.cartItems[e.id]}</p>
-//                                 <img src={HighlightOffIcon} onClick={() => { removefromcart(e.id) }} alt='' />
-//                             </div>
-//                             <hr />
-//                         </div>
-                    
-//                 } else {
-//                     return <>Hello hi</>; // Return null if the condition is not met
-//                 }
-//             })}
-//         </div>
-//     );
-// };
-
-// export default Cartitems;
-
-
-
-import React from 'react'
-
-const Cartitems = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
 
-export default Cartitems
+      <hr />
+      {console.log("From CartItem Page", items)}
+      {Data.map((e) => {
+        // if (cartItems.cartItems[e.id] > 0) {
+        if (items.includes(e.id)) {
+          return (
+            <div>
+              {e.offerPrice}
+              <div className="cartitems-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.Name}</p>
+                <p> ₹{e.offerPrice}</p>
+                <button className="cartitems-quantity">{e.id}</button>
+                {/* <p>{e.offerPrice * cartItems.cartItems[e.id]}</p> */}
+                <img
+                  src={HighlightOffIcon}
+                  // onClick={() => {
+                  //   removefromcart(e.id);
+                  // }}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        } else {
+          return <>Hello hi</>; // Return null if the condition is not met
+        }
+      })}
+    </div>
+  );
+};
+
+export default Cartitems;
+
+// import React from 'react'
+
+// const Cartitems = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default Cartitems
--- src/Pages/Products/Single.jsx
+++ src/Pages/Products/Single.jsx
@@ -1,130 +1,181 @@
-import React from 'react';
-import './Single.css';
-import { useParams, } from 'react-router-dom'; //allows to access parameters from the URL
-import { Data } from '../../Components/Data';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import FlashOnIcon from '@mui/icons-material/FlashOn';
+import React from "react";
+import "./Single.css";
+import { useParams } from "react-router-dom"; //allows to access parameters from the URL
+import { Data } from "../../Components/Data";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FlashOnIcon from "@mui/icons-material/FlashOn";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import StarBorderPurple500Icon from '@mui/icons-material/StarBorderPurple500';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import {LocalOffer as Badge} from '@mui/icons-material';
-import { useLocation } from 'react-router-dom';
-import { useEffect} from 'react';
-import {useState} from 'react';
-import { Table,TableBody,TableRow,TableCell } from '@mui/material';
-import PinDropIcon from '@mui/icons-material/PinDrop';
-import Cartitems from '../../Components/Cartitems/Cartitems';
-
+import StarBorderPurple500Icon from "@mui/icons-material/StarBorderPurple500";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { LocalOffer as Badge } from "@mui/icons-material";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Table, TableBody, TableRow, TableCell } from "@mui/material";
+import PinDropIcon from "@mui/icons-material/PinDrop";
+import Cartitems from "../../Components/Cartitems/Cartitems";
 
 const Single = () => {
   const location = useLocation();
-    useEffect(() => {
-        if (!location.hash) {
-            window.scrollTo(0, 0);
-        }
-    }, [location]);
-  
+  useEffect(() => {
+    if (!location.hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
   // Create Empty cart
-  const getDefaultCart = ()=>{
+  const getDefaultCart = () => {
     let cart = {};
-    for(let index =0; index < Data.length +1;index++){
+    for (let index = 0; index < Data.length + 1; index++) {
       cart[index] = 0;
     }
     return cart;
-  }
-
-  const[cartItems,setcartItems] = useState(getDefaultCart());
-
+  };
   const { id } = useParams();
-  const date = new Date(new Date().getTime()+(5*24*60*60*1000))
-
-  
+  const date = new Date(new Date().getTime() + 5 * 24 * 60 * 60 * 1000);
 
   // add to cart
-  const addtocart = (itemid) =>{
-    setcartItems((prev)=>({...prev,[itemid]:prev[itemid]+1}))
+  const addtocart = (itemid) => {
+    let cartItemString = localStorage.getItem("items");
+    let cartItemArray = JSON.parse(cartItemString);
+    console.log("Test: ", cartItemArray);
+    if (!cartItemArray) {
+      localStorage.setItem("items", JSON.stringify([itemid]));
+    } else if (cartItemArray.length && !cartItemArray.includes(itemid)) {
+      localStorage.setItem("items", JSON.stringify([...cartItemArray, itemid]));
+    }
+    // localStorage.setItem("items", [itemid]);
     // console.log(cartItems);
-  }
+  };
 
   //remove from cart
 
-  const removefromcart = (itemid) =>{
-    setcartItems((prev)=>({...prev,[itemid]:prev[itemid]-1}))
-  }
-
   // Find the item in Data array based on id
-  const selectedItem = Data.find(item => item.id === parseInt(id));
+  const selectedItem = Data.find((item) => item.id === parseInt(id));
   // console.log(cartitems);
 
-  // If selectedItem is not found, handle it 
+  // If selectedItem is not found, handle it
   if (!selectedItem) {
     return <div>Item not found!</div>;
   }
 
-  
-  
-
   return (
-    <div className='single-outer'>
-      <div className='single-inner'>
-        <div className='single-pic'>
-          
-          <div className='single-picture'>
-          <div className='sin-heart'>
-            <FavoriteIcon />
+    <div className="single-outer">
+      <div className="single-inner">
+        <div className="single-pic">
+          <div className="single-picture">
+            <div className="sin-heart">
+              <FavoriteIcon />
+            </div>
+            <img src={selectedItem.image} className="sing-img" alt="" />
           </div>
-            <img src={selectedItem.image} className='sing-img' alt='' />  
-          </div>
-          <div className='butto-div'>
-            <button onClick={()=>{addtocart(selectedItem.id)}} className='si-butt-1' ><ShoppingCartIcon/>Add to cart</button>
-            <button className='si-butt-2'><FlashOnIcon/>Buy Now</button>
+          <div className="butto-div">
+            <button
+              onClick={() => {
+                addtocart(selectedItem.id);
+              }}
+              className="si-butt-1"
+            >
+              <Link to="/cart">
+                <ShoppingCartIcon />
+                Add to cart
+              </Link>
+            </button>
+
+            <button className="si-butt-2">
+              <FlashOnIcon />
+              Buy Now
+            </button>
           </div>
         </div>
-        <div className='single-det'>
-          <p className='sin-para'>{selectedItem.Name}</p>
-          <p className='sin-desc'>{selectedItem.about}</p>
-          <p style={{color:'green',fontSize:'100%'}}>Special Price</p>
-          <div className='sin-rate'>
-            
-            <p className='sin-span1-1'>₹{selectedItem.offerPrice}</p>
-            <p className='sin-span2-2'>₹{selectedItem.actualPrice}</p>
-            <p className='sin-span2-3'>&nbsp;&nbsp;{selectedItem.off} </p>
-            <p style={{backgroundcolor:'white',marginTop:'3%',marginLeft:'1%',opacity:'0.3',}}><InfoOutlinedIcon/></p>
+        <div className="single-det">
+          <p className="sin-para">{selectedItem.Name}</p>
+          <p className="sin-desc">{selectedItem.about}</p>
+          <p style={{ color: "green", fontSize: "100%" }}>Special Price</p>
+          <div className="sin-rate">
+            <p className="sin-span1-1">₹{selectedItem.offerPrice}</p>
+            <p className="sin-span2-2">₹{selectedItem.actualPrice}</p>
+            <p className="sin-span2-3">&nbsp;&nbsp;{selectedItem.off} </p>
+            <p
+              style={{
+                backgroundcolor: "white",
+                marginTop: "3%",
+                marginLeft: "1%",
+                opacity: "0.3",
+              }}
+            >
+              <InfoOutlinedIcon />
+            </p>
           </div>
-          <div className='rating-sec'>
-            <button className='rati-box'>4.1<StarBorderPurple500Icon fontSize='40%'/></button>
-            <p className='ra-tex'>170 Ratings & 12 Reviews</p>
+          <div className="rating-sec">
+            <button className="rati-box">
+              4.1
+              <StarBorderPurple500Icon fontSize="40%" />
+            </button>
+            <p className="ra-tex">170 Ratings & 12 Reviews</p>
           </div>
-          <p className='sin-avai'>Available Offers</p>
-          <p className='ty-1'><Badge fontSize='small' sx={{color:'#41A317'}}/>&nbsp;&nbsp;<b>Bank Offer</b> Get ₹25* instant discount for the 1st Flipkart Order using Flipkart UPI</p>
-          <p className='ty-1'><Badge fontSize='small' sx={{color:'#41A317'}}/>&nbsp;&nbsp;<b>Bank Offer</b> 5% Cashback on Flipkart Axis Bank Card</p>
-          <p className='ty-1'><Badge fontSize='small' sx={{color:'#41A317'}}/>&nbsp;&nbsp;<b>Special Price</b> Get extra 40% off (price inclusive of cashback/coupon)</p>
-          <p className='ty-1'><Badge fontSize='small' sx={{color:'#41A317'}}/>&nbsp;&nbsp;<b>Partner Offer</b> Sign-up for Shopee Pay Later & get free Times Prime Benefits<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; worth ₹10,000*</p>
+          <p className="sin-avai">Available Offers</p>
+          <p className="ty-1">
+            <Badge fontSize="small" sx={{ color: "#41A317" }} />
+            &nbsp;&nbsp;<b>Bank Offer</b> Get ₹25* instant discount for the 1st
+            Flipkart Order using Flipkart UPI
+          </p>
+          <p className="ty-1">
+            <Badge fontSize="small" sx={{ color: "#41A317" }} />
+            &nbsp;&nbsp;<b>Bank Offer</b> 5% Cashback on Flipkart Axis Bank Card
+          </p>
+          <p className="ty-1">
+            <Badge fontSize="small" sx={{ color: "#41A317" }} />
+            &nbsp;&nbsp;<b>Special Price</b> Get extra 40% off (price inclusive
+            of cashback/coupon)
+          </p>
+          <p className="ty-1">
+            <Badge fontSize="small" sx={{ color: "#41A317" }} />
+            &nbsp;&nbsp;<b>Partner Offer</b> Sign-up for Shopee Pay Later & get
+            free Times Prime Benefits
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; worth ₹10,000*
+          </p>
 
           <Table>
             <TableBody>
               <TableRow>
-                <TableCell style={{color: '#878787'}}><PinDropIcon/>Deliver to</TableCell>
-                <TableCell style={{fontWeight: 600}}>Delivery by {date.toDateString()} |₹40</TableCell>
+                <TableCell style={{ color: "#878787" }}>
+                  <PinDropIcon />
+                  Deliver to
+                </TableCell>
+                <TableCell style={{ fontWeight: 600 }}>
+                  Delivery by {date.toDateString()} |₹40
+                </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell style={{color: '#878787'}}>Warranty</TableCell>
-                <TableCell > 6 Months</TableCell>
+                <TableCell style={{ color: "#878787" }}>Warranty</TableCell>
+                <TableCell> 6 Months</TableCell>
               </TableRow>
               <TableRow>
-                <TableCell style={{color: '#878787'}}>Seller</TableCell>
-                <TableCell > 
-                  <p style={{color: '#2874f0', fontWeight: 600, fontSize: '17px'}}>HSAtlastrade</p>
-                  <p style={{fontWeight: 500}}>&diams; 10 Days Return Policy</p>
-                  <p style={{fontWeight: 500}}>&diams; GST Invoice Available</p>
+                <TableCell style={{ color: "#878787" }}>Seller</TableCell>
+                <TableCell>
+                  <p
+                    style={{
+                      color: "#2874f0",
+                      fontWeight: 600,
+                      fontSize: "17px",
+                    }}
+                  >
+                    HSAtlastrade
+                  </p>
+                  <p style={{ fontWeight: 500 }}>
+                    &diams; 10 Days Return Policy
+                  </p>
+                  <p style={{ fontWeight: 500 }}>
+                    &diams; GST Invoice Available
+                  </p>
                 </TableCell>
               </TableRow>
             </TableBody>
-
           </Table>
         </div>
       </div>
-      <Cartitems cartItems={cartItems} removefromcart={removefromcart}  />
     </div>
   );
 };
