f0f76dcad328dd95103d45122da646cbcd5b201b
Subhratanu <subhratanu.jisce@gmail.com>
2024-03-25 13:08:15 +0530

    Empty Cart Dev Cpmpleted

--- src/Components/Cartitems/Cartitems.jsx
+++ src/Components/Cartitems/Cartitems.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Cartitems.css";
 import { Data } from "../Data";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Cartitems = () => {
   /* Setting up cartItem Array State Variable */
@@ -64,88 +64,106 @@ const Cartitems = () => {
   }, []);
 
   return (
-    <div className="cartitems">
-      <div className="cartitems-format-main">
-        <p>Products</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-      </div>
+    <>
+      {items == 0 ? (
+        <div>
+          <h1 className="empty-cart">Your Shopee cart is empty </h1>
+          <Link to="/">
+            <h1 className="left-off">Pick up where you left off</h1>
+          </Link>
+        </div>
+      ) : (
+        <div className="cartitems">
+          <div className="cartitems-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+          </div>
 
-      <hr />
-      {Data.map((e) => {
-        if (items.includes(e.id)) {
-          if (typeof e.offerPrice === "string") {
-            e.offerPrice = parseInt(e.offerPrice.replace(",", ""));
-          }
+          <hr />
+          {Data.map((e) => {
+            if (items.includes(e.id)) {
+              if (typeof e.offerPrice === "string") {
+                e.offerPrice = parseInt(e.offerPrice.replace(",", ""));
+              }
 
-          return (
-            <div key={e.id}>
-              <div className="cartitems-format cartitems-format-main">
-                <img
-                  src={e.image}
-                  alt="cart-item"
-                  className="carticon-product-icon"
-                  height={60}
-                  width={60}
-                />
-                <p>{e.Name}</p>
-                <p> ₹{e.offerPrice}</p>
-                <div className="cartitems-quantity">
-                  {count(items, e.id) > 0 ? (
-                    <button
-                      onClick={() => decrementCounter(e.id)}
-                      className="dec-butt"
-                    >
-                      -
-                    </button>
-                  ) : (
-                    ""
-                  )}
+              return (
+                <div key={e.id}>
+                  <div className="cartitems-format cartitems-format-main">
+                    <img
+                      src={e.image}
+                      alt="cart-item"
+                      className="carticon-product-icon"
+                      height={60}
+                      width={60}
+                    />
+                    <p>{e.Name}</p>
+                    <p> ₹{e.offerPrice}</p>
+                    <div className="cartitems-quantity">
+                      {count(items, e.id) > 0 ? (
+                        <button
+                          onClick={() => decrementCounter(e.id)}
+                          className="dec-butt"
+                        >
+                          -
+                        </button>
+                      ) : (
+                        ""
+                      )}
 
-                  <p className="p-valu">{count(items, e.id)}</p>
+                      <p className="p-valu">{count(items, e.id)}</p>
 
-                  <button onClick={() => incrementCounter(e.id)}>+</button>
+                      <button
+                        onClick={() => incrementCounter(e.id)}
+                        className="inc-butt"
+                      >
+                        +
+                      </button>
+                    </div>
+                    <p>{count(items, e.id) * e.offerPrice}</p>
+                  </div>
+                  <hr />
                 </div>
-                <p>{count(items, e.id) * e.offerPrice}</p>
+              );
+            }
+            return null;
+          })}
+          <div className="cartitems-down">
+            <div className="cartitems-total">
+              <h1>Cart Totals</h1>
+              <div>
+                <div className="cartitems-total-item">
+                  <p>Subtotal</p>
+                  <p>{calculateTotal()}</p>{" "}
+                  {/* Calculate total cart item price */}
+                </div>
+                <hr />
+                <div className="cartitems-total-item">
+                  <p>Shipping Fee</p>
+                  <p>Free</p>
+                </div>
+                <hr />
+                <div className="cartitems-total-item">
+                  <h3>Total</h3>
+                  <h3>{calculateTotal()}</h3>{" "}
+                  {/* Calculate total cart item price */}
+                </div>
               </div>
-              <hr />
+              <button>Proceed to checkout</button>
             </div>
-          );
-        }
-        return null;
-      })}
-      <div className="cartitems-down">
-        <div className="cartitems-total">
-          <h1>Cart Totals</h1>
-          <div>
-            <div className="cartitems-total-item">
-              <p>Subtotal</p>
-              <p>{calculateTotal()}</p> {/* Calculate total cart item price */}
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <p>Shipping Fee</p>
-              <p>Free</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <h3>Total</h3>
-              <h3>{calculateTotal()}</h3> {/* Calculate total cart item price */}
+            <div className="cartitems-promocode">
+              <p>If you have a promo code,Enter It here</p>
+              <div className="cartitems-promobox">
+                <input type="text" placeholder="promo code" />
+                <button>submit</button>
+              </div>
             </div>
           </div>
-          <button>Proceed to checkout</button>
         </div>
-        <div className="cartitems-promocode">
-          <p>If you have a promo code,Enter It here</p>
-          <div className="cartitems-promobox">
-            <input type="text" placeholder="promo code" />
-            <button>submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
