cb54b110fcd44f0fa2dc1bd54c0db72f5c606350
Upasana Chattopadhyay <chattopadhyayupasana2@gmail.com>
2024-03-27 12:06:10 +0530

    Account page corrected

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -64,17 +64,17 @@ const Account = () => {
             <h3>Personal Informations</h3>
             <form className='profile-form'>
               <div className='profile-name'>
-                <input type='text' placeholder='First Name' className='profile-first-name'></input>
-                <input type='text' placeholder='Last Name' className='profile-last-name'></input>
+                <div className='profile-first-name'><p>First Name</p></div>
+                <div  className='profile-last-name'><p>Last Name</p></div>
               </div>
               {/* <p className='profile-email'>Email Address</p> */}
               <h3 className='profile-heading'>Email Address</h3>
               <div className='profile-name'>
-                <input type='text' placeholder='Email Address' className='profile-email'></input>
+                <div  className='profile-email'><p>Email Address</p></div>
               </div>
               <h3 className='profile-heading'>Mobile Number</h3>
               <div className='profile-name'>
-                <input type='text' placeholder='Mobile Number' className='profile-email'></input>
+                <div className='profile-email'><p>Mobile Number</p></div>
               </div>
               
             </form>
@@ -98,13 +98,13 @@ const Account = () => {
             <h3>FAQs</h3>
             <br/>
             <b><p>Why is my card being tokenised?</p></b>
-            <p className='faq-ans'>As per the new RBI guidelines to make card data more secure, merchants like Flipkart cannot store the card details of users. As an alternative, RBI has authorised card networks and card issuers to offer card tokenisation services, which means the replacement of actual credit and debit card details with an alternate code called “token”. The user can either choose to tokenise their card by giving consent for future transactions or choose to continue without tokenisation.</p>
+            <p className='faq-ans'>As per the new RBI guidelines to make card data more secure, merchants like Shopee cannot store the card details of users. As an alternative, RBI has authorised card networks and card issuers to offer card tokenisation services, which means the replacement of actual credit and debit card details with an alternate code called “token”. The user can either choose to tokenise their card by giving consent for future transactions or choose to continue without tokenisation.</p>
             <br/>
             <b><p>What is a token?</p></b>
-            <p className='faq-ans'>A token is generated when a user gives consent to Flipkart to tokenise their card. A token is a unique value for a combination of card, token requestor (Flipkart is a token requestor & accepts request from the customer for tokenisation of a card and passes it onto the card network to issue a corresponding token) and device. The token does not contain any personal information linked to your card and is generated only when a customer uses a new card for a successful transaction on Flipkart.</p>
+            <p className='faq-ans'>A token is generated when a user gives consent to Shopee to tokenise their card. A token is a unique value for a combination of card, token requestor (Shopee is a token requestor & accepts request from the customer for tokenisation of a card and passes it onto the card network to issue a corresponding token) and device. The token does not contain any personal information linked to your card and is generated only when a customer uses a new card for a successful transaction on Shopee.</p>
             <br/>
             <b><p>Is it safe to tokenise my card?</p></b>
-            <p className='faq-ans'>Yes. A tokenised card transaction is considered safer as the actual card details are not shared with the Flipkart during transaction processing . Card information is stored with the authorised card networks or card issuers only and Flipkart does not store your 16-digit card number.</p>
+            <p className='faq-ans'>Yes. A tokenised card transaction is considered safer as the actual card details are not shared with the Shopee during transaction processing . Card information is stored with the authorised card networks or card issuers only and Shopee does not store your 16-digit card number.</p>
             <br/>
             <b><p>Is tokenisation of card mandatory?</p></b>
             <p className='faq-ans'>No, customer can choose whether or not to tokenise their card.</p>
