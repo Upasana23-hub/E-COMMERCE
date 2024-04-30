4f876605965a502d8b80e7f9de08a0bb443fefa4
girianimesh <animeshgiri98839828@gmail.com>
2024-04-30 23:11:36 +0530

    size removed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,31 +3,45 @@ import React from "react";
 import "./Card.css";
 // import { Link } from "react-router-dom";
 
-export const Card = ({ Name, image, about, offerPrice, actualPrice, size,off }) => {
+export const Card = ({
+  Name,
+  image,
+  about,
+  offerPrice,
+  actualPrice,
+  size,
+  off,
+}) => {
   return (
     // <Link to={`single/${.id}`}>
-      <div className="card">
-          <button className="card-heart"><FavoriteIcon /></button>
-        <div className="card__image">
-          <img src={image} alt="images" />
-        </div>
-        <div className="productDet">
-          <div className="card__details">
-            <p className="para">{Name}</p>
-            <p className="desc">{about}</p>
-            <div className="rate">
-              <p className="span1">₹{offerPrice}</p>
-              <p className="span2">₹{actualPrice}</p>
-              <p className="span3" >{off}</p>
-            </div>
+    <div className="card">
+      <button className="card-heart">
+        <FavoriteIcon />
+      </button>
+      <div className="card__image">
+        <img src={image} alt="images" />
+      </div>
+      <div className="productDet">
+        <div className="card__details">
+          <p className="para">{Name}</p>
+          <p className="desc">{about}</p>
+          <div className="rate">
+            <p className="span1">₹{offerPrice}</p>
+            <p className="span2">₹{actualPrice}</p>
+            <p className="span3">{off}</p>
           </div>
-          <div className="card__size">
+        </div>
+        <div className="card__size">
+          {size ? (
             <p>
               size <p className="sizing"> {size}</p>
             </p>
-          </div>
+          ) : (
+            " "
+          )}
         </div>
       </div>
+    </div>
     // </Link>
   );
 };
